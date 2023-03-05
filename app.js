2a819e07629cbf80e1e83a62c2a05a7faa70ea94
Prithvi101 <pruthvik41@hotmail.com>
2023-03-05 20:03:46 +0530

    Added Work Profile

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
 let toDo = []
+let toDoWork = []
 let days = ['Sunday','Monday','Tuesday','Wenesday','Thursday','Friday','Saturday']
 let months = ['January','Feburary','March','April','May','June','July','August','September','October','November','December']
 
@@ -18,12 +19,26 @@ app.get('/',(req,res) => {
         newListItems:toDo
     })
 })
+app.get('/work',(req,res) => {
+    res.render('list',{
+        kindOfDay:'WORK',
+        newListItems:toDoWork
+    })
+})
 app.post('/',(req,res)=>{
     if(req.body.newItem.length != 0){
-        toDo.push(req.body.newItem);
+        console.log(req.body.newItem.value );
+        if(req.body.newNodebtn === "WORK"){
+            toDoWork.push(req.body.newItem);
+            res.redirect('/work')
+        }
+        else{
+            toDo.push(req.body.newItem);
+            res.redirect('/')
+        }
     }
    
-    res.redirect('/')
+    
 })
 
 
